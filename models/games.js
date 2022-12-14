0f71fe85dc3de14d7d899799094deeea31502ce0
Christopher Anderson <tophermichaelson@gmail.com>
2022-12-13 22:47:14 -0500

    what is happening

--- models/games.js
+++ models/games.js
@@ -1,11 +1,7 @@
 const mongoose = require('mongoose')
 
 const gamesSchema = new mongoose.Schema ({
-<<<<<<< HEAD
     name : "String",
-=======
-    name: "String",
->>>>>>> 5b285602c6cec9d77fb355a27820126c0fdd4d35
     list : [{}],
 
 })
