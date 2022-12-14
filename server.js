edee528c1c0b56523388d97e72be0c43b7bc5bf5
Christopher Anderson <tophermichaelson@gmail.com>
2022-12-14 16:41:28 -0500

    added 2nd schema and routes

--- server.js
+++ server.js
@@ -46,10 +46,41 @@ app.use(express.json());// returns middleware that only parses JSON - may or may
 // Schema 
 
 const Games = require('./models/games.js')
-
+const Review = require('./models/review.js')
 //___________________
 // Routes
 //___________________
+
+//___________________
+// Routes
+//____________
+
+    app.post('/reviews', (req, res) => {
+      Review.create(req.body, (err, createdReviews) => {
+        res.json(createdReviews)
+      })
+    });
+
+    app.get('/reviews', (req, res) => {
+      Review.find({}, (err, foundList) => {
+        res.json(foundList)
+      })
+    })
+
+    app.delete('/reviews/:id', (req, res) => {
+      Review.findByIdAndRemove(req.params.id, (err, deletedReview) => {
+        res.json(deletedReveiw)
+      })
+    })
+
+    app.put('/reviews/:id', (req, res) => {
+      Review.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updatedReview) => {
+        res.json(updatedReview)
+      })
+    })
+
+    
+ 
 
 
     app.post('/games', (req, res) => {
@@ -58,27 +89,27 @@ const Games = require('./models/games.js')
         res.json(createdGames)
         })
     })
-   
-                             //get route grabbing all data
+
+                   //get route grabbing all data
    app.get('/games', (req, res) => {
      Games.find({}, (err, foundList) => {
        res.json(foundList)
      })
    })
-   
-                             //delete route..
+
+                        //delete route..
    app.delete('/games/:id', (req, res) => {
      Games.findByIdAndRemove(req.params.id, (err, deletedGames) => {
        res.json(deletedGames)
      })
    })
-   
-                             //put route
+                       //put route
    app.put('/games/:id', (req, res) => {
-     Games.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updatedGames) => {
+Games.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updatedGames) => {
        res.json(updatedGames)
      })
    })
+
 //___________________
 //Listener
 //___________________
